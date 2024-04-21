907d823da39a78cf445a1ee4ac8468eedd905937
zhihao-qiu <zhihao.qiu@hotmail.com>
2024-04-20 19:46:43 -0700

    import tailwindcss

--- src/ListPage.js
+++ src/ListPage.js
@@ -1,15 +1,16 @@
 import './App.css';
 import { Link } from 'react-router-dom';
 
+
 function ListPage() {
 
   return (
     <div className="list-group">
       <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/ThreeDHover">3D Hover</Link></li>
-        <li><Link to="/SimpleMap">Simple Canada Map</Link></li>
-        <li><Link to="/BgImgScrolling">Background Image Scrolling</Link></li>
+        <li className='mb-2'><Link to="/" className="inline-block bg-blue-500 hover:bg-blue-600 text-white font-semibold w-60 py-2 px-4 rounded-full">Home</Link></li>
+        <li className='mb-2'><Link to="/ThreeDHover" className="inline-block bg-blue-500 hover:bg-blue-600 text-white font-semibold w-60 py-2 px-4 rounded-full">3D Hover</Link></li>
+        <li className='mb-2'><Link to="/SimpleMap" className="inline-block bg-blue-500 hover:bg-blue-600 text-white font-semibold w-60 py-2 px-4 rounded-full">Simple Canada Map</Link></li>
+        <li className='mb-2'><Link to="/BgImgScrolling" className="inline-block bg-blue-500 hover:bg-blue-600 text-white font-semibold w-60 py-2 px-4 rounded-full">Background Image Scrolling</Link></li>
       </ul>
     </div>
   );
