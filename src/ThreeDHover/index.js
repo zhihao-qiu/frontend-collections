6cc8dc6fc72eab8c02e2ca0044e3182df2ee649f
zhihao-qiu <zhihao.qiu@hotmail.com>
2024-04-21 23:47:39 -0700

    adjusted page layout

--- src/ThreeDHover/index.js
+++ src/ThreeDHover/index.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import '../App.css';
 import './3dhover.css';
 import card from './pic/promo_swsh061.jpg';
-
 function ThreedHover() {
   const [rotation, setRotation] = useState({ rx: 0, ry: 0 });
 
@@ -23,17 +22,23 @@ function ThreedHover() {
     return (value / length) * (range[1] - range[0]) + range[0];
   }
   return (
-    <div className='App-header'>
-      <div
-        className="card"
-        style={{
-          '--rx': `${rotation.rx}deg`,
-          '--ry': `${rotation.ry}deg`
-        }}
-        onMouseMove={handleMouseOver}
-        onMouseLeave={handleMouseLeave}
-      >
-        <img src={card} alt="" />
+    <div className="w-full h-screen flex flex-col justify-start items-center">
+      <div>
+        <p className="text-4xl font-bold mt-10">3D Hover Card</p>
+        <p className="text-2xl mt-10">Hover over the card to see the 3D effect</p>
+      </div>
+      <div className='h-full flex justify-center items-center'>
+        <div
+          className="card"
+          style={{
+            '--rx': `${rotation.rx}deg`,
+            '--ry': `${rotation.ry}deg`
+          }}
+          onMouseMove={handleMouseOver}
+          onMouseLeave={handleMouseLeave}
+        >
+          <img src={card} alt="" />
+        </div>
       </div>
     </div>
   );
