56a0d7a08a4df18964738b61c9880b425945d757
zhihao-qiu <zhihao.qiu@hotmail.com>
2024-04-23 16:10:38 -0700

    using tailwindcss to instead css

--- src/ListPage.js
+++ src/ListPage.js
@@ -5,15 +5,12 @@ import { Link } from 'react-router-dom';
 function ListPage() {
 
   return (
-    <div className="list-group">
-      <ul>
-        <li className='mb-2'><Link to="/" className="inline-block bg-blue-500 hover:bg-blue-600 text-white font-semibold w-60 py-2 px-4 rounded-full">Home</Link></li>
-        <li className='mb-2'><Link to="/ThreeDHover" className="inline-block bg-blue-500 hover:bg-blue-600 text-white font-semibold w-60 py-2 px-4 rounded-full">3D Hover</Link></li>
-        <li className='mb-2'><Link to="/SimpleMap" className="inline-block bg-blue-500 hover:bg-blue-600 text-white font-semibold w-60 py-2 px-4 rounded-full">Simple Canada Map</Link></li>
-        <li className='mb-2'><Link to="/BgImgScrolling" className="inline-block bg-blue-500 hover:bg-blue-600 text-white font-semibold w-60 py-2 px-4 rounded-full">Background Image Scrolling</Link></li>
-        <li className='mb-2'><Link to="/ClipPath" className="inline-block bg-blue-500 hover:bg-blue-600 text-white font-semibold w-60 py-2 px-4 rounded-full">ClipPath</Link></li>
-      </ul>
-    </div>
+    <div className="h-screen flex flex-col items-center bg-gray-100">
+      <div className='mb-4 mt-4'><Link to="/" className="inline-block bg-blue-500 hover:bg-blue-600 text-white font-semibold w-60 py-2 px-4 rounded-full">3D Hover</Link></div>
+      <div className='mb-4'><Link to="/SimpleMap" className="inline-block bg-blue-500 hover:bg-blue-600 text-white font-semibold w-60 py-2 px-4 rounded-full">Simple Canada Map</Link></div>
+      <div className='mb-4'><Link to="/BgImgScrolling" className="inline-block bg-blue-500 hover:bg-blue-600 text-white font-semibold w-60 py-2 px-4 rounded-full">Background Image Scrolling</Link></div >
+      <div className='mb-4'><Link to="/ClipPath" className="inline-block bg-blue-500 hover:bg-blue-600 text-white font-semibold w-60 py-2 px-4 rounded-full">ClipPath</Link></div >
+    </div >
   );
 }
 
