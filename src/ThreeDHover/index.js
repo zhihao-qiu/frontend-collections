683b160f8ee43a63c0797b9875c41133b4d8c48a
zhihao-qiu <zhihao.qiu@hotmail.com>
2024-04-23 16:11:35 -0700

    update layout of ThreeDHover

--- src/ThreeDHover/index.js
+++ src/ThreeDHover/index.js
@@ -1,8 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../App.css';
 import './3dhover.css';
 import card from './pic/promo_swsh061.jpg';
-function ThreedHover() {
+const ThreedHover = ({ setTopic }) => {
+  useEffect(() => {
+    setTopic('ThreedHover');
+  }, [setTopic]);
+
   const [rotation, setRotation] = useState({ rx: 0, ry: 0 });
 
   const yRange = [-20, 20];
@@ -22,11 +26,7 @@ function ThreedHover() {
     return (value / length) * (range[1] - range[0]) + range[0];
   }
   return (
-    <div className="w-full h-screen flex flex-col justify-start items-center">
-      <div>
-        <p className="text-4xl font-bold mt-10">3D Hover Card</p>
-        <p className="text-2xl mt-10">Hover over the card to see the 3D effect</p>
-      </div>
+    <div className="w-full mt-32">
       <div className='h-full flex justify-center items-center'>
         <div
           className="card"
@@ -42,6 +42,6 @@ function ThreedHover() {
       </div>
     </div>
   );
-}
+};
 
 export default ThreedHover;
