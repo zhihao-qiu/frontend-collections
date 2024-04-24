7e3011ba2796f4095f397d9cece9bfd329a2c78f
zhihao-qiu <zhihao.qiu@hotmail.com>
2024-04-23 21:49:07 -0700

    SmileRatings project init

--- src/ListPage.js
+++ src/ListPage.js
@@ -9,7 +9,8 @@ function ListPage() {
       <div className='mb-4 mt-4'><Link to="/" className="inline-block bg-blue-500 hover:bg-blue-600 text-white font-semibold w-60 py-2 px-4 rounded-full">3D Hover</Link></div>
       <div className='mb-4'><Link to="/SimpleMap" className="inline-block bg-blue-500 hover:bg-blue-600 text-white font-semibold w-60 py-2 px-4 rounded-full">Simple Canada Map</Link></div>
       <div className='mb-4'><Link to="/BgImgScrolling" className="inline-block bg-blue-500 hover:bg-blue-600 text-white font-semibold w-60 py-2 px-4 rounded-full">Background Image Scrolling</Link></div >
-      <div className='mb-4'><Link to="/ClipPath" className="inline-block bg-blue-500 hover:bg-blue-600 text-white font-semibold w-60 py-2 px-4 rounded-full">ClipPath</Link></div >
+      <div className='mb-4'><Link to="/ClipPath" className="inline-block bg-blue-500 hover:bg-blue-600 text-white font-semibold w-60 py-2 px-4 rounded-full">ClipPath</Link></div>
+      <div className='mb-4'><Link to="/SmileRatings" className="inline-block bg-blue-500 hover:bg-blue-600 text-white font-semibold w-60 py-2 px-4 rounded-full">Smile Ratings</Link></div>
     </div >
   );
 }
