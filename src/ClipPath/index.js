2976e8260f4457845713de664e4edbd9af98fe61
zhihao-qiu <zhihao.qiu@hotmail.com>
2024-04-23 21:06:09 -0700

    updated layout of Clippath

--- src/ClipPath/index.js
+++ src/ClipPath/index.js
@@ -1,14 +1,16 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
 import '../App.css';
 import './ClipPath.css';
 import city from './pic/city.jpeg';
 import baby from './pic/baby.jpeg';
-import horse from './pic/horse.jpeg';
 
-function ClipPath() {
+const ClipPath = ({ setTopic }) => {
+  useEffect(() => {
+    setTopic('Clippath');
+  }, [setTopic]);
 
   return (
-    <div>
+    <div className='flex flex-row'>
       <div className='container1'>
         <img src={city} alt="city" className='box1' />
       </div>
@@ -20,6 +22,6 @@ function ClipPath() {
       </div>
     </div>
   );
-}
+};
 
 export default ClipPath;
