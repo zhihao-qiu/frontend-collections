6237974a762ae782b4e697a66e9af3d8131c1e57
zhihao-qiu <zhihao.qiu@hotmail.com>
2024-04-30 21:28:20 -0700

    Apply mui for ThreeDHover

--- src/ThreeDHover/index.js
+++ src/ThreeDHover/index.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import '../App.css';
 import './3dhover.css';
 import card from './pic/promo_swsh061.jpg';
+import { Box } from '@mui/system';
+
 const ThreedHover = ({ setTopic }) => {
   useEffect(() => {
     setTopic('ThreedHover');
@@ -26,21 +28,27 @@ const ThreedHover = ({ setTopic }) => {
     return (value / length) * (range[1] - range[0]) + range[0];
   }
   return (
-    <div className="w-full mt-32">
-      <div className='h-full flex justify-center items-center'>
-        <div
-          className="card"
-          style={{
-            '--rx': `${rotation.rx}deg`,
-            '--ry': `${rotation.ry}deg`
-          }}
-          onMouseMove={handleMouseOver}
-          onMouseLeave={handleMouseLeave}
-        >
-          <img src={card} alt="" />
-        </div>
-      </div>
-    </div>
+    <Box sx={
+      {
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: '100px',
+      }}>
+      <Box className="card"
+        style={{
+          '--rx': `${rotation.rx}deg`,
+          '--ry': `${rotation.ry}deg`
+        }}
+        onMouseMove={handleMouseOver}
+        onMouseLeave={handleMouseLeave}
+      >
+        <img src={card} alt="" />
+      </Box>
+
+    </Box>
   );
 };
 
